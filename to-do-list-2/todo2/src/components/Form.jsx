84de5fe5d269a778c4f11680dc3774f2486d0573
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2023-12-03 09:29:54 -0300

    update

--- to-do-list-2/todo2/src/components/Form.jsx
+++ to-do-list-2/todo2/src/components/Form.jsx
@@ -1,11 +1,26 @@
 import { Button, Paper, TextField } from "@mui/material";
+import { useState } from "react";
 
-const Form = () => {
+const Form = ({ addTodo }) => {
+  const [text, setText] = useState(null);
+  const [id, setId] = useState(0)
+
+  const todoCreate = (text) => {
+    const todoObj = {text: text, id: id };
+    setId(id + 1);
+    addTodo(todoObj)
+  }
+
   return (
     <Paper style={{ padding: "1em" }}>
         <div style={{ display: "flex", justifyContent: "center" }}>
-            <TextField id="outlined-basic" label="Tarefa" variant="outlined" fullWidth />
-            <Button variant="text">Add</Button>
+            <TextField 
+              id="outlined-basic" 
+              label="Tarefa" 
+              variant="outlined" 
+              onChange={(e) => setText(e.target.value)} fullWidth 
+            />
+            <Button variant="text" onClick={() => todoCreate(text)}>Add</Button>
         </div>
     </Paper>
   );
--- to-do-list-2/todo2/src/pages/Home.jsx
+++ to-do-list-2/todo2/src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import { Container, List } from "@mui/material";
 import TodoItem from "../components/TodoItem";
 import Form from "../components/Form";
+import { useState } from "react";
 
 const Home = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (id) => {
+    console.log(id);
+  };
+
   return (
     <Container maxWidth="xs" style={{ marginTop: "1em" }}>
-       <Form />
-       <List sx={{ bgcolor: "background.paper", marginTop: "1em" }}>
-          <TodoItem />
+       <Form addTodo={addTodo} />
+       <List sx={{ marginTop: "1em" }}>
+          {todos.map((todo) => (
+            <div key={todo.id} style={{ marginTop: "1em" }}>
+              <TodoItem todo={todo} deleteTodo={deleteTodo} />
+            </div>
+          ))}
        </List>
       </Container>
   );
