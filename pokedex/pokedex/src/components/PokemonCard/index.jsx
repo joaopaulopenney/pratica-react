626e03097c7ae74ee5fe7781a7ba6006adc80019
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-01-10 23:56:32 -0300

    update pokedex

--- pokedex/pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/pokedex/src/components/PokemonCard/index.jsx
@@ -6,7 +6,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function PokemonCard() {
+export default function PokemonCard({ name }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -16,7 +16,7 @@ export default function PokemonCard() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {name}
         </Typography>
         {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
--- pokedex/pokedex/src/pages/Home.jsx
+++ pokedex/pokedex/src/pages/Home.jsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { Container, Grid } from '@mui/material';
 import Navbar from '../components/Navbar';
 import PokemonCard from '../components/PokemonCard';
 
 const Home = () => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=50")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons()
+  }, []);
+
+  const getPokemons = () => {
+    var endpoints = []
+    for ( var i = 1; i < 50; i++ ) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    }
+    var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+    return response;
+    // axios
+    //   .get("https://pokeapi.co/api/v2/pokemon?limit=50")
+    //   .then((res) => setPokemons(res.data.results))
+    //   .catch((err) => console.log(err));
+  };
   return (
     <div>
       <Navbar />
         <Container maxWidth="false">
           <Grid container>
-            <Grid item xs={3}>
-              <PokemonCard />
-            </Grid>
-            <Grid item xs={3}>
-              <PokemonCard />
-            </Grid>
-            <Grid item xs={3}>
-              <PokemonCard />
-            </Grid>
-            <Grid item xs={3}>
-              <PokemonCard />
-            </Grid>
+            {pokemons.map((pokemon, key) => (
+              <Grid item xs={3} key={key}>
+                <PokemonCard name={pokemon.name} />
+              </Grid>
+            ))}
           </Grid>
         </Container>
     </div>
