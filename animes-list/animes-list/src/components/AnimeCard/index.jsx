fcedef28f2a7c82ceac9ab5b7b71294b20a51b29
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-01-20 09:33:47 -0300

    update animes-list

---
+++ animes-list/animes-list/src/components/AnimeCard/index.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function AnimeCard({ title, image, score, genres }) {
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="350"
+          image={image}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Score: {score}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Genres: 
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ animes-list/animes-list/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Box, Container, Grid } from '@mui/material';
+import AnimeCard from '../components/AnimeCard';
+
+export const Home = () => {
+
+    const [animes, setAnimes] = useState([]);
+
+    const baseUrl = "https://api.jikan.moe/v4";
+    // const characterUrl = `/anime/${id}/characters`;
+    // const staffUrl = `/anime/${id}/staff`;
+    // const episodeUrl = `/anime/${id}/episodes`;
+    // const newsUrl = `/anime/${id}/news`;
+    // const forumUrl = `/anime/${id}/forum`;
+    // const videosUrl = `/anime/${id}/videos`;
+    // const pictureUrl = `/anime/${id}/pictures`;
+
+    const getAnimes = () => {
+        axios
+            .get(`${baseUrl}/anime`)
+            .then((res) => setAnimes(res.data.data))
+            .catch((err) => console.log(err))
+    }
+
+    useEffect(() => {
+        getAnimes();
+    }, []);
+
+    console.log(animes)
+
+  return (
+    <>
+        <div>Navbar</div>
+        <Container>
+            <Grid container spacing={2}>
+                {animes.map((anime, key) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={key}>
+                        <Box>
+                            <AnimeCard title={anime.title} image={anime.images.jpg.large_image_url} score={anime.score} genres={anime.genres}  />
+                        </Box>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    </>
+  )
+};
