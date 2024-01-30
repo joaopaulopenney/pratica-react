4f90d4562b01b6912b6537c6c32c17785e31f145
João Paulo Penney <137862398+joaopaulopenney@users.noreply.github.com>
2024-01-30 10:49:46 -0300

    update animes-list

--- animes-list/animes-list/src/pages/Profile.jsx
+++ animes-list/animes-list/src/pages/Profile.jsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Navbar from '../components/Navbar';
 import { useNavigate } from 'react-router-dom';
+import { Box, Container } from '@mui/material';
 
 export function Profile({ animeData }) {
   const navigate = useNavigate();
@@ -24,32 +25,41 @@ export function Profile({ animeData }) {
   return (
     <>
       <Navbar hideSearch />
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          component="img"
-          alt={animeData.title}
-          height="140"
-          image={animeData.images.jpg.large_image_url}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {animeData.title}
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" component="div">
-            Rated: {animeData.rating}
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" component="div">
-            Score: {animeData.score}
-          </Typography>
-          <Typography gutterBottom variant="subtitle1" component="div">
-            Episodes: {animeData.episodes}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="large" sx={{ width: "100px", backgroundColor: "#1976D2", color: "white", margin: "auto", ":hover": { opacity: "0.7", backgroundColor: "#1976D2", color: "white" } }} onClick={() => navigate("/")}>Back</Button>
+      <Container sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", }}>
+        <Card sx={{ maxWidth: 350, minWidth: 300}}>
+          <CardMedia
+            component="img"
+            alt={animeData.title}
+            height="300px"
+            image={animeData.images.jpg.large_image_url}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {animeData.title}
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              Rated: {animeData.rating}
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              Score: {animeData.score}
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              Episodes: {animeData.episodes}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="large" sx={{ width: "100px", backgroundColor: "#1976D2", color: "white", margin: "auto", ":hover": { opacity: "0.7", backgroundColor: "#1976D2", color: "white" } }} onClick={() => navigate("/")}>Back</Button>
 
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: "700px" }}>
+          <CardContent sx={{ height: "530px" }}>
+            <Typography gutterBottom variant="body2" component="div">
+              {animeData.synopsis}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 }
